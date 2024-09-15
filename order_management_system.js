69e4c5fc89f7ba97e653e8703aa684fd1e06f405
ZachRober <nolethedgehogdie@gmail.com>
2024-09-15 16:24:01 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,4 @@
+const inventory = [{name: icecream, price: 3.49, quantity:20},
+    {name: cheesecake, price: 6.99, quantity:5},
+    {name: rootbeer, price: 1.49, quantity:7},
+    {name: oreos, price: 3.99, quantity:15}];
